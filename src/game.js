6d95104f3bd4fc0ed28749ef2ee1a2cc1ef018d9
Jeff Dam <jeff.dam@gmail.com>
2019-05-16 17:02:33 -0700

    Render gems to stack on top of each other

--- src/game.js
+++ src/game.js
@@ -3,20 +3,36 @@ const Gems = require('./gems')
 class Game {
   constructor(ctx) {
     this.ctx = ctx;
+    this.renderGem = this.renderGem.bind(this);
+    this.gems = [new Gems({ pos: { x: 150, y: 0 }, colors: this.randomGemColors(), prevHeight:600 })];
+    this.gemsFalling = false;
   }
 
   randomGemColors() {
-    const colors = ["#ff0000", "#0000ff", "#08d123", "#fce305"];
+    const colors = ["#ff0000", "#0000ff", "#3c9600", "#f4d041"];
     return [colors[Math.floor(Math.random() * 4)], colors[Math.floor(Math.random() * 4)]];
   }
 
-  gameStart() {
-    const gem = new Gems({ pos: { x: 150, y: 0 }, colors: this.randomGemColors() });
-    gem.render(this.ctx);
+  renderGem() {
+    let id = requestAnimationFrame(this.renderGem);
+    this.gems.slice(this.gems.length-1)[0].drop(this.ctx, id);
+    this.gems.slice(0, this.gems.length-1).forEach(gem => {
+      gem.render(this.ctx);
+    });
+    if (this.gems.slice(this.gems.length - 1)[0].vel === 0) {
+      const prevHeight = (this.gems.slice(this.gems.length - 1)[0].gem1.pos.y - 50);
+      console.log(prevHeight);
+
+      this.gems.push(new Gems({ pos: { x: 150, y: 0 }, colors: this.randomGemColors(), prevHeight: prevHeight}));
+      if (prevHeight > -50) {
+        this.renderGem();
+      }
+    }
   }
-
-
-
+  
+  gameStart() {
+    this.renderGem();
+  }
  
 }
 
--- src/gems.js
+++ src/gems.js
@@ -1,35 +1,50 @@
 class Gems {
-  constructor({ pos, colors }) {
-    this.pos = pos,
-    this.size = { width: 50, height: 50 }
-    this.colors = colors;
-    // this.drop = this.drop.bind(this);
-    // this.render = this.render.bind(this);
+  constructor({ pos, colors, prevHeight }) {
+    this.gem1 = {
+      pos: pos,
+      color: colors[0]
+    };
+    this.gem2 = {
+      pos: {x: pos.x, y: pos.y +50},
+      color: colors[1]
+    };
+    this.width = 50;
+    this.height = 50;
+    this.vel = 5;
+    this.prevHeight = prevHeight;
+  }
+
+  handleCollision(id) {
+    if (this.gem1.pos.y >= this.prevHeight || this.gem2.pos.y >= this.prevHeight) {
+      this.vel = 0;
+      cancelAnimationFrame(id);
+    }
   }
 
   render(ctx) {
-    ctx.fillStyle = this.colors[0];
+    ctx.fillStyle = this.gem1.color;
     ctx.fillRect(
-      this.pos.x,
-      this.pos.y,
-      this.size.width,
-      this.size.height
+      this.gem1.pos.x,
+      this.gem1.pos.y,
+      this.width,
+      this.height
       );
-    ctx.fillStyle = this.colors[1];
+    ctx.fillStyle = this.gem2.color;
     ctx.fillRect(
-      this.pos.x,
-      this.pos.y + 50,
-      this.size.width,
-      this.size.height
+      this.gem2.pos.x,
+      this.gem2.pos.y,
+      this.width,
+      this.height
       );
   }
 
-  // drop(ctx) {
-  //   requestAnimationFrame(this.drop(ctx));
-  //   ctx.clearRect(0,0, 300, 650);
-  //   this.pos = { x: this.pos.x, y: this.pos.y + 1 };
-  //   this.render(ctx);
-  // }
+  drop(ctx, id) {
+    this.handleCollision(id);
+    ctx.clearRect(0,0, 300, 650);
+    this.gem1.pos.y += this.vel;
+    this.gem2.pos.y += this.vel;
+    this.render(ctx);
+  }
 
 }
 
